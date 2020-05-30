68343b512eb3cdab89d172cb772c557d18491ff9
ThreeHDM <juanbilardi@gmail.com>
2020-05-30 12:43:40 -0300

    Refactorizamos number wizard, comenzamos a agregar quit game button

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//agregamos el namespace de textMeshPro
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
     //Serializamos las variables. Esto hace que pueda ver y manipular los valores de estas variables en la Interfaz de unity
     [SerializeField] int max;
     [SerializeField] int min;
+
+    //Serializamos un campo que permite conectar el guess con el texto de la UI. El type es el tipo de objeto usado en la interface de Unity. En este caso TextMeshProUGUI. Lo llamamos guessText. Al asignar el GuessText Object con el GamePlay Object podemos manipular lo que se muestra en ese texto con exte script
+    [SerializeField] TextMeshProUGUI guessText;
+
     int guess;
 
     // Use this for initialization
@@ -18,26 +24,33 @@ public class NumberWizard : MonoBehaviour
     void StartGame()
     {
         //Max and min son asignados en el GameObject
-        max = max + 1;
+
         //Inicializamos con un guess definido
-        guess = (max + min) / 2;
+        //Aquí es la primera vez que mostramos el texto. Asignamos a la propiedad text de guessText la variable guess convertida a string con ToString()
+        NextGuess();
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        //sumamos uno para que no adivine un numero que ya mostró
+        min = guess + 1;
         NextGuess();
     }
 
     public void OnPressLower()
-    {
-        max = guess;
+    {   
+        //restamos uno para que no adivine un numero que ya mostró
+        max = guess - 1;
         NextGuess();
     }
 
     
     void NextGuess()
     {
-        guess = (max + min) / 2;
+        //guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
+        //actualizamos lo que mostramos en la Interfaz del juego
+        guessText.text = guess.ToString();
     }
+
 }

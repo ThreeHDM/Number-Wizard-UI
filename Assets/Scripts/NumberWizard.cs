f05adfca67350673a5e57dac64d22f21fd69c8f2
ThreeHDM <juanbilardi@gmail.com>
2020-05-30 11:58:47 -0300

    Importamos y refactorizamos el código de Numbert Wizard

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    //Serializamos las variables. Esto hace que pueda ver y manipular los valores de estas variables en la Interfaz de unity
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        //Max and min son asignados en el GameObject
+        max = max + 1;
+        //Inicializamos con un guess definido
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
